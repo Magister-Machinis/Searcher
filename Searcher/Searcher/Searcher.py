50973418075c19432a4ba9801360457a4991d36b
Magister Machinis <30908581+Magister-Machinis@users.noreply.github.com>
2020-07-01 22:23:54 -0400

    nearly done sorting integration of speciifc match protocol, need to get it to not break everything else too

--- Searcher/Searcher/Searcher.py
+++ Searcher/Searcher/Searcher.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 #initialize class with a search query and an option to print self.debug info to console,
 #then strings or json passed to '__Matched' function will be compared to the query 
@@ -26,7 +27,7 @@ import re
 
 
 class searcher(object):    
-    
+
     def __init__(self, inputstring,debug=False):
         self.parsedsearch=[]
         self.debug=debug
@@ -97,50 +98,7 @@ class searcher(object):
         if self.debug:
             print(f"query processed as {self.parsedsearch}")
     
-#user presenting wrapper to specific match option
-def SpecificMatch(self, data):
-    return [item for item in _innerSpecificMatch(self, data)]
-#ensures that results are cleared from object
-    def _innerSpecificMatch(self, data):
-        self.data=[data]
-        
-        if self.debug:
-            print(f"Checking query against {data}")
-                
-        try:
-            for item in self._SMatch():
-                yield item
-        finally:
-            self.data=None
-            
-            
-        
-    #general loop to iterate through object down to leaf items
-    def _SMatch(self, data= self.data):
-        if isinstance(data, list) or isinstance(data, tuple):
-            for item in data:
-               if self.debug:
-                    print(f"iterating down to {item}")
-               yield _SMatch(item)
-        elif isinstance(data, dict):
-            for key in data:
-                if self.debug:
-                    print(f"iterating down to {data[key]}")
-                yield _SMatch(data[key])
-        else:
-           yield _SChecker(str(data))
-
-    #sends data to the _Match() function to see if it fits, then yields up if it does
-    def _SChecker(data,count=0):
-        self.data.append(data)
-        if self.debug:
-            print(f"Checking if {data} is a match")
-        if _Match(subdata=len(self.data)):
-            if self.debug:
-                print("Matched!")
-            yield data
-            
-    
+    #wrapper for general match protocol
     def IsMatch(self, data,subdata=0):        
         self.data=[data]        
         if self.debug:
@@ -150,13 +108,11 @@ def SpecificMatch(self, data):
         if self.debug:
             print(f"final truth is {truth}")
         return truth
-        
-
+    
     #iterates through the saved search arranges the string comparisons between assorted ANDs and ORs,
     #inverting where a NOT indicates to do such or dropping into a subsearch where () had indicated, 
-    #then initiates the __checking function when a string is iterated over to initiate a search of the data passed in
-    
-    def __Match(self, count=0, truth=None,subdata=0):
+    #then initiates the __checking function when a string is iterated over to initiate a search of the data passed in    
+    def __Match(self, count=0, truth=None,subdata=None):
         if self.debug:
             print(f"current truth is {truth}")
         nottest =True
@@ -170,24 +126,24 @@ def SpecificMatch(self, data):
                     count+=1
 
             if(self.parsedsearch[count] == 1):
-                    temp,count = self.__Match(count+1,truth=truth,subdata=subdata)
+                    temp,count = self.__Match(count=count+1,truth=truth,subdata=subdata)
                     if(truth == None):
                         truth=temp
                     else:
                         truth = truth and temp
 
             elif(self.parsedsearch[count] == 2):
-                    temp,count = self.__Match(count+1,truth=truth,subdata=subdata)
+                    temp,count = self.__Match(count=count+1,truth=truth,subdata=subdata)
                     if(truth == None):
                         truth=temp
                     else:
                         truth = truth or temp
         
             elif(isinstance(self.parsedsearch[count],str)):
-                    truth = self.__check(count,subdata=subdata) and nottest
+                    truth = self.__check(count=count,subdata=subdata) and nottest
                     count+=1
             elif(isinstance(self.parsedsearch[count],re.Pattern)):
-                    truth = self.__check(count,subdata=subdata) and nottest
+                    truth = self.__check(count=count,subdata=subdata) and nottest
                     count+=1
             elif("searcher" in str((self.parsedsearch[count]))):
                     truth =  self.parsedsearch[count].IsMatch(self.data,subdata=subdata) and nottest     
@@ -197,14 +153,16 @@ def SpecificMatch(self, data):
             print(f"returning truth as {truth}")
         return bool(truth), int(count)
 
-#now that a string to compare has been identified, this iterates through the data to determine if it is present or not
+    #now that a string to compare has been identified, this iterates through the data to determine if it is present or not
     def __check(self,count,data =None,subdata=0):
-        if self.debug:
-            print(f"checking {self.parsedsearch[count]} against {data}")
+        
         checker = False
-        if data ==None:
-                data= self.data[subdata]
-            
+        if subdata !=None:
+                data= subdata
+        elif data == None:
+            data=self.data
+        if self.debug:
+            print(f"checking {self.parsedsearch[count]} against {data}")    
         if isinstance(data, list) or isinstance (data,tuple):
             for item in data:
                 if(self.__check(count, item)):
@@ -228,3 +186,59 @@ def SpecificMatch(self, data):
                         print("Match!")
                     checker = True
         return checker
+
+    #ensures that results are cleared from object
+    def _innerSpecificMatch(self,data):
+        if isinstance(data, list):
+            self.data=data    
+        else:
+            self.data=[data]
+         
+        if self.debug:
+            print(f"Checking query against {data}")                
+        try:
+            for item in self._SMatch(self.data):
+                yield item
+        except Exception as e:
+            raise e
+        finally:
+            self.data=None
+
+    #sends data to the _Match() function to see if it fits, then yields up if it does
+    def _SChecker(self,data,count=0):
+        self.data.append(data)
+        if self.debug:
+            print(f"Checking if {data} is a match")
+            print(f'data set is {self.data}')
+            print(f'{len(self.data)}')
+        truth,count=self.__Match(subdata=self.data[-1])
+        del self.data[-1]
+        if truth:
+            if self.debug:
+                print("Matched!")
+            yield data
+
+    #general loop to iterate through object down to leaf items
+    def _SMatch(self, data):               
+        if self.debug:
+            print(f'reviewing {data}')
+            print(type(data))
+        if isinstance(data, list) or isinstance(data, tuple):            
+            for item in data:
+                for subitem in self._SMatch(item):
+                    yield subitem
+        elif isinstance(data, dict):            
+            print(list(data))
+            for key in list(data):
+                for item in self._SMatch(data[key]):
+                    yield item          
+        else:
+            for item in self._SChecker(str(data)):
+                yield item
+
+    #user presenting wrapper to specific match option
+    def SpecificMatch(self, data):  
+         data = [test for test in (item for item in self._innerSpecificMatch(data))]
+         if self.debug:
+            print(f"results are {data}")
+         return data
--- Searcher/Searcher/unittests.py
+++ Searcher/Searcher/unittests.py
@@ -2,6 +2,89 @@ import unittest
 from Searcher import Searcher
 import json
 
+class Searcher_tests_No_debug(unittest.TestCase):
+    #sample json chunk for testing purposes
+    examplejson= """{
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}"""
+    examplejsondes=json.loads(examplejson)
+    
+    def testtruenodeb(self):
+        search = Searcher.searcher('"SGML" AND "example"')
+        self.assertTrue(search.IsMatch(self.examplejsondes))
+    
+    def testfalsenodeb(self):
+        search = Searcher.searcher('"SGML" AND NOT "example"')
+        self.assertFalse(search.IsMatch(self.examplejsondes))
+
+    def testsubqueriesnodeb(self):
+        search = Searcher.searcher('"SGML" AND ("example" OR "meta")')
+        self.assertTrue(search.IsMatch(self.examplejsondes))
+
+    def testsubqueriesfalsenodeb(self):
+        search = Searcher.searcher('"SGML" AND NOT ("example" OR "meta")')
+        self.assertFalse(search.IsMatch(self.examplejsondes))
+
+    #testing the specific match protocol
+    def testtrueSpecnodeb(self):
+        search = Searcher.searcher('"Markup" AND "Language"')
+        data = search.SpecificMatch(self.examplejsondes)
+        print(data)
+        for item in data:
+            print(item)
+        self.assertEqual(len(data), 1)
+        self.assertTrue(isinstance(data,list))
+        self.assertTrue(isinstance(data[0],str))
+
+    def testtrueregSpecnodeb(self):
+        search = Searcher.searcher(' ("Markup" AND "Language") AND R"Stand(a|A)rd"')
+        data = search.SpecificMatch(self.examplejsondes)
+        print(data)
+        for item in data:
+            print(item)
+        self.assertEqual(len(data), 1)
+        self.assertTrue(isinstance(data,list))
+        self.assertTrue(isinstance(data[0],str))
+
+    def testfalseSpecnodeb(self):
+        search = Searcher.searcher('"Standard" AND "used" AND R"ISO"')
+        data = search.SpecificMatch(self.examplejsondes)
+        print(data)
+        for item in data:
+            print(item)
+        self.assertEqual(len(data), 0)
+        self.assertTrue(isinstance(data,list))        
+    
+    def testfalseregSpecnodeb(self):
+        search = Searcher.searcher('"Standard" AND "used"')
+        data = search.SpecificMatch(self.examplejsondes)
+        print(data)
+        for item in data:
+            print(item)
+        self.assertEqual(len(data), 0)
+        self.assertTrue(isinstance(data,list))
+
+    
+
 class Searcher_tests(unittest.TestCase):
 
 #sample json chunk for testing purposes
@@ -28,6 +111,8 @@ class Searcher_tests(unittest.TestCase):
     }
 }"""
     examplejsondes=json.loads(examplejson)
+    
+    
 
     #testing the general match protocol
     def testtrue(self):
@@ -36,31 +121,18 @@ class Searcher_tests(unittest.TestCase):
 
     def testfalse(self):
         search = Searcher.searcher('"SGML" AND NOT "example"',True)
-        self.assertFalse(search.IsMatch(self.examplejsondes))
-
-    def testtruenodeb(self):
-        search = Searcher.searcher('"SGML" AND "example"')
-        self.assertTrue(search.IsMatch(self.examplejsondes))
-
-    def testfalsenodeb(self):
-        search = Searcher.searcher('"SGML" AND NOT "example"')
-        self.assertFalse(search.IsMatch(self.examplejsondes))
+        self.assertFalse(search.IsMatch(self.examplejsondes))   
 
     def testsubqueries(self):
         search = Searcher.searcher('"SGML" AND ("example" OR "meta")', True)
         self.assertTrue(search.IsMatch(self.examplejsondes))
     
-    def testsubqueriesnodeb(self):
-        search = Searcher.searcher('"SGML" AND ("example" OR "meta")')
-        self.assertTrue(search.IsMatch(self.examplejsondes))
+    
 
     def testsubqueriesfalse(self):
         search = Searcher.searcher('"SGML" AND NOT ("example" OR "meta")', True)
-        self.assertFalse(search.IsMatch(self.examplejsondes))
+        self.assertFalse(search.IsMatch(self.examplejsondes))  
     
-    def testsubqueriesfalsenodeb(self):
-        search = Searcher.searcher('"SGML" AND NOT ("example" OR "meta")')
-        self.assertFalse(search.IsMatch(self.examplejsondes))
 
     def testregexFalse(self):
         search = Searcher.searcher('"SGML" AND NOT ("example" OR "meta") AND R"Stand(a|A)rd"', True)
@@ -73,7 +145,43 @@ class Searcher_tests(unittest.TestCase):
     #testing the specific match protocol
     def testtrueSpec(self):
         search = Searcher.searcher('"Markup" AND "Language"',True)
-        self.assertTrue(search.SpecificMatch(self.examplejsondes))
+        data = search.SpecificMatch(self.examplejsondes)
+        print(data)
+        for item in data:
+            print(item)
+        self.assertEqual(len(data), 1)
+        self.assertTrue(isinstance(data,list))
+        self.assertTrue(isinstance(data[0],str))
+
+    def testtrueregSpec(self):
+        search = Searcher.searcher(' ("Markup" AND "Language") AND R"Stand(a|A)rd"',True)
+        data = search.SpecificMatch(self.examplejsondes)
+        print(data)
+        for item in data:
+            print(item)
+        self.assertEqual(len(data), 1)
+        self.assertTrue(isinstance(data,list))
+        self.assertTrue(isinstance(data[0],str))
+
+    def testfalseSpec(self):
+        search = Searcher.searcher('"Standard" AND "used" AND R"ISO"',True)
+        data = search.SpecificMatch(self.examplejsondes)
+        print(data)
+        for item in data:
+            print(item)
+        self.assertEqual(len(data), 0)
+        self.assertTrue(isinstance(data,list))
+        
+    
+    def testfalseregSpec(self):
+        search = Searcher.searcher('"Standard" AND "used"',True)
+        data = search.SpecificMatch(self.examplejsondes)
+        print(data)
+        for item in data:
+            print(item)
+        self.assertEqual(len(data), 0)
+        self.assertTrue(isinstance(data,list))
+        
 
 
     
